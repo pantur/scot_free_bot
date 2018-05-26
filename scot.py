7378eb5bae859a6f04334430c2a8373d1f0e559a
rathsud <sudhanshu.s.rath@gmail.com>
2018-05-25 22:14:45 -0400

    v.0.0.5

--- scot.py
+++ scot.py
@@ -10,54 +10,66 @@
      Can't let them get away scotch-free!
 '''
 from __future__ import print_function
+
 try:
     import praw
     import time
 except ImportError as ie:
     print('ErrorImportingModules')
+    print(ie)
 try:
     from ResConfig import debugFlag as DEBUG, \
-         MAX_ERROR_COUNT, LONG_SLEEP, \
-         usr, pwd, sec, clid, desc
+        MAX_ERROR_COUNT, SHORT_SLEEP, \
+        LONG_SLEEP, usr, pwd, sec, \
+        clid, desc
 except ImportError as configError:
     print('UnableToImportConfig')
+    print(configError)
+
 
 def connectToReddit():
     global CONN
     try:
-        CONN = praw.Reddit(user_agent=desc, \
-                               client_id=clid, \
-                               client_secret=sec, \
-                               username=usr, \
-                               password=pwd)
+        CONN = praw.Reddit(user_agent=desc,
+                           client_id=clid,
+                           client_secret=sec,
+                           username=usr,
+                           password=pwd)
     except Exception as ConnectionError:
         print(ConnectionError)
         return False
     return True
 
+
 def findViolations():
-    for comment in CONN.subreddit('all').stream.comments():
-        if 'fuck' in str(comment.body.encode('utf-8')):
+    for comment in CONN.subreddit('test').stream.comments():
+        if 'scotch-free' in str(comment.body.encode('utf-8')):
             print(comment.body.encode('utf-8'))
 
-def pauseBot(len):
+
+def pauseBot(len_):
     if DEBUG:
         print('Sleeping for some time...')
-
-    if len == 1:
+    
+    if len_ == 1:
         time.sleep(SHORT_SLEEP)
-    elif len == 2:
+    elif len_ == 2:
         time.sleep(LONG_SLEEP)
     else:
         print('InvalidSleepTime')
         return False
-
+    
     if DEBUG:
         print('Woke up from sleeping!')
 
     return True
 
-if __name__=='__main__':
+
+def recordTotalViolationCount():
+    pass
+
+
+if __name__ == '__main__':
     ERROR_COUNT = 0
     if DEBUG:
         print('user: ' + usr)
