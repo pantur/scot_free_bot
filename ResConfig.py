2494a1a856c6247abd8c4b1ce6f947cbd357da61
rathsud <sudhanshu.s.rath@gmail.com>
2018-06-05 19:38:11 -0400

    res-stuff

---
+++ ResConfig.py
@@ -0,0 +1,20 @@
+usr = 'ITS_SCOT_FREE'
+pwd = 'lokill'
+sec = 'opGjnaezEcm0tzBh2e2HpfdY30A'
+clid = 'm3EDIIGOyD9_DA'
+desc = 'Scot Free Bot'
+
+debugFlag = True
+
+MAX_ERROR_COUNT = 1000
+LONG_SLEEP = 500
+SHORT_SLEEP = 15
+
+MESSAGE = 'Hello, {}' + '! ' \
+            + 'I am afraid I cannot let you' \
+            + ' get away here!' \
+            + ' It\'s spelled **scot-free**, my good' \
+            + ' Redditor! Have a nice day!'
+
+
+
